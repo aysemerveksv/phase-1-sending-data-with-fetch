4a90e888fe81799b3d75d98d8bccc2814787cd42
Ayşe Merve Kosova <mervekosova1@hotmail.com>
2023-08-18 16:21:03 +0300

    Implement submitData function for POST requests

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+function submitData(userName, userEmail) {
+  const formData = {
+    name: userName,
+    email: userEmail
+  };
+
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(response => response.json())
+    .then(data => {
+      // Append the new id to the DOM
+      const idElement = document.createElement("p");
+      idElement.textContent = data.id;
+      document.body.appendChild(idElement);
+    })
+    .catch(error => {
+      // Display error message on the DOM
+      const errorElement = document.createElement("p");
+      errorElement.textContent = error.message;
+      document.body.appendChild(errorElement);
+    });
+}
+
+// Call the submitData function
+submitData("John Doe", "john@example.com");
